64356f097da5ecb364cd407dcd65b14eed8adcea
José Leonardo Zúñiga Hernández <114005429+Leonides2@users.noreply.github.com>
2024-09-22 19:57:55 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace FirstApp
+{
+    public class Program
+    {
+        
+        static void Main(string[] args){
+            
+            foreach(string n in args){
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
